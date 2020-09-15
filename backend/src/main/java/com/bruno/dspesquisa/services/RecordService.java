4f497492b3b58e99fc5dfe30ab0d733a761242c4
Bruno Pereira <brunopereira.19@hotmail.com>
2020-09-15 01:38:59 -0300

    GET records

--- backend/src/main/java/com/bruno/dspesquisa/services/RecordService.java
+++ backend/src/main/java/com/bruno/dspesquisa/services/RecordService.java
@@ -3,6 +3,8 @@ package com.bruno.dspesquisa.services;
 import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,24 +17,29 @@ import com.bruno.dspesquisa.repository.RecordRepository;
 
 @Service
 public class RecordService {
-	
+
 	@Autowired
 	private RecordRepository recordRepository;
-	
+
 	@Autowired
 	private GameRepository gameRepository;
-	
+
 	@Transactional
 	public RecordDTO insert(RecordInsertDTO dto) {
 		Record entity = new Record();
 		entity.setName(dto.getName());
 		entity.setAge(dto.getAge());
 		entity.setMoment(Instant.now());
-		
+
 		Game game = gameRepository.getOne(dto.getGameId());
 		entity.setGame(game);
 		entity = recordRepository.save(entity);
 		return new RecordDTO(entity);
-		
+
+	}
+
+	@Transactional(readOnly = true)
+	public Page<RecordDTO> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+		return recordRepository.findByMoments(minDate, maxDate, pageRequest).map(e -> new RecordDTO(e));
 	}
 }
