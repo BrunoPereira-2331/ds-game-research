cc691c8395c1fa29b2d2f95e13781c168f291fb6
Bruno Pereira <brunopereira.19@hotmail.com>
2020-09-16 16:14:29 -0300

    criando navegacao com rotas

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
+import Routes from './routes';
 import './App.css';
-import Header from './components/Header'
-import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <Home></Home>
-    </div>
-  );
+    <Routes />
+    );
 }
 
 export default App;
